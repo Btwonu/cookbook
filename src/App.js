5604e3bc341140a9bd76bdaf2814679b4f8f9c1a
Val Delchev <btwonu@gmail.com>
2021-04-26 12:43:52 +0300

    remove materialize, unneeded components and integrate tailwind

--- src/App.js
+++ src/App.js
@@ -1,80 +1,11 @@
-// Base
-import { Component } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import './css/App.css';
 import './css/main.scss';
 
-// Services
-import service from './services/recipeService';
-
-// Components
-import Container from './components/Container/Container';
-import Card from './components/Card/Card';
-import Category from './components/Category/Category';
-
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    service.getAll().then((recipes) => {
-      const lunchRecipes = recipes.filter((r) => r.category == 'lunch');
-      const breakfastRecipes = recipes.filter((r) => r.category == 'breakfast');
-      const desertRecipes = recipes.filter((r) => r.category == 'desert');
-
-      this.setState({
-        lunchRecipes,
-        breakfastRecipes,
-        desertRecipes,
-      });
-    });
-  }
-
-  render() {
-    return (
-      <Container>
-        <Category name="Breakfast">
-          {this.state.breakfastRecipes?.map((recipe) => (
-            <Card
-              key={recipe.id}
-              title={recipe.title}
-              image={recipe.image}
-              createdAt={recipe.createdAt}
-              chef={recipe.chef}
-              category={recipe.category}
-            />
-          ))}
-        </Category>
-
-        <Category name="Lunch">
-          {this.state.lunchRecipes?.map((recipe) => (
-            <Card
-              key={recipe.id}
-              title={recipe.title}
-              image={recipe.image}
-              createdAt={recipe.createdAt}
-              chef={recipe.chef}
-              category={recipe.category}
-            />
-          ))}
-        </Category>
-        <Category name="Desert">
-          {this.state.desertRecipes?.map((recipe) => (
-            <Card
-              key={recipe.id}
-              title={recipe.title}
-              image={recipe.image}
-              createdAt={recipe.createdAt}
-              chef={recipe.chef}
-              category={recipe.category}
-            />
-          ))}
-        </Category>
-      </Container>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <h1>Hella</h1>
+    </>
+  );
+};
 
 export default App;
--- src/components/Container/Container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Container(props) {
-  return <div className="container">{props.children}</div>;
-}
-
-export default Container;
