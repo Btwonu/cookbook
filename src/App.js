f2ca019087a3b7d4b8869e92a4fa1c600dbc68de
Val Delchev <btwonu@gmail.com>
2021-05-19 11:05:27 +0300

    add axios dep

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './css/main.scss';
 import { ThemeProvider } from './contexts/ThemeContext';
+import axios from 'axios';
 
 import Landing from './pages/Landing';
 import Recipe from './pages/Recipe';
 import Auth from './pages/Auth';
 import CreateRecipe from './pages/CreateRecipe';
 import RecipeDetails from './components/RecipeDetails';
+
+if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.NODE_ENV);
+  axios.defaults.baseURL = 'http://localhost:5000';
+} else if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV);
+
+  // axios.defaults.baseURL =
+  // 'https://europe-west1-movie-find-dev.cloudfunctions.net/api';
+}
 
 const App = () => {
   return (
