30328285203a102754e94c6f803ae3892eadb078
Val Delchev <btwonu@gmail.com>
2021-06-16 10:53:23 +0300

    tweak landing's cta

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -12,14 +12,14 @@ const Landing = () => {
       <section className="pt-24">
         <div className="container mx-auto flex px-16  md:flex-row flex-col items-center">
           <div className=" md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
+            <h1 className="title-font text-3xl sm:text-4xl sm:font-medium lg:text-5xl mb-4 ">
               Organize your recipes
             </h1>
-            <p className="mb-8">
+            <p className="mb-8 text-sm sm:text-md leading-relaxed xl:text-lg">
               Keep track of your favorite recipes, find new ones, communicate
               with other chefs and more.
             </p>
-            <div className="flex justify-center w-full">
+            <div className="flex justify-center w-full md:justify-start">
               <Link to="/auth" className="btn">
                 Sign up now
               </Link>
