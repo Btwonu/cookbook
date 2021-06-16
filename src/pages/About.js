9d2c4dc60b160f03367ec4544cfcef82e57b078a
Val Delchev <btwonu@gmail.com>
2021-06-16 11:16:51 +0300

    create about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,33 @@
 import Layout from './layouts/Layout';
 
 const About = () => {
-  return <Layout>About</Layout>;
+  return (
+    <Layout>
+      <article className="max-w-screen-sm m-auto leading-loose">
+        <h2 className="text-2xl text-center pt-6">About</h2>
+        <p className="p-6">
+          Cheese slices who moved my cheese cream cheese. Monterey jack cut the
+          cheese cheese on toast lancashire danish fontina blue castello red
+          leicester swiss. Manchego brie everyone loves emmental bocconcini
+          cheesy feet mozzarella cheesy grin. Goat parmesan swiss cheese strings
+          red leicester paneer chalk and cheese chalk and cheese. Jarlsberg feta
+          port-salut brie camembert de normandie monterey jack goat cow.
+          Lancashire cheesecake smelly cheese.
+        </p>
+        <p className="p-6">
+          Cauliflower cheese mascarpone melted cheese. Swiss pecorino chalk and
+          cheese mascarpone when the cheese comes out everybody's happy parmesan
+          airedale cheese triangles. Queso cheesecake stinking bishop fondue
+          monterey jack mozzarella caerphilly airedale. Cheesy grin who moved my
+          cheese croque monsieur blue castello pepper jack edam cheese and
+          biscuits the big cheese. Cheesy feet croque monsieur cream cheese feta
+          bocconcini cheesy feet the big cheese babybel. Monterey jack camembert
+          de normandie say cheese goat smelly cheese stilton cheese strings
+          pecorino. Fromage gouda.
+        </p>
+      </article>
+    </Layout>
+  );
 };
 
 export default About;
