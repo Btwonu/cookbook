ad783bb767fcf783902e434aae6b448734d9b925
Val Delchev <btwonu@gmail.com>
2021-06-21 18:02:04 +0300

    add initial shopping list styles

--- src/pages/Shopping.js
+++ src/pages/Shopping.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import Button from '../components/Button';
 import Layout from './layouts/Layout';
 
+import { MdClose } from 'react-icons/md';
+
 const products = [
   {
     name: 'cheese',
@@ -12,6 +14,7 @@ const products = [
 function Shopping() {
   return (
     <Layout>
+      <h2 className="text-center text-2xl p-6">Shopping List</h2>
       <ShoppingList />
     </Layout>
   );
@@ -43,12 +46,12 @@ function ShoppingList() {
   };
 
   const productsList = products.map((product, index) => {
-    return <li key={index}>{product.name}</li>;
+    return <Product key={index} name={product.name} />;
   });
 
   return (
-    <>
-      <ul>{productsList}</ul>
+    <section className="border max-w-screen-md mx-auto flex flex-col items-center p-10">
+      <ul className="w-4/5 p-4 text-lg">{productsList}</ul>
       <input
         type="text"
         onChange={onInputChange}
@@ -56,7 +59,19 @@ function ShoppingList() {
         onKeyDown={onInputKeyPress}
       />
       <Button>Save</Button>
-    </>
+    </section>
+  );
+}
+
+function Product({ name }) {
+  return (
+    <li className="flex justify-between items-center">
+      <div className="flex items-center gap-4">
+        <input type="checkbox" />
+        <p>{name}</p>
+      </div>
+      <MdClose />
+    </li>
   );
 }
 
