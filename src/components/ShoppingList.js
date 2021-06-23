14d62e4fcdfebb5aa318c696d7b325fe51bbb107
Val Delchev <btwonu@gmail.com>
2021-06-23 11:10:07 +0300

    add initial shopping list spinner

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 import Product from './Product';
 import Button from './Button';
+import Spinner from 'react-spinkit';
 
 function ShoppingList({
   products,
@@ -10,6 +13,7 @@ function ShoppingList({
   productValue,
   saveShoppingList,
 }) {
+  const [isLoading, setIsLoading] = useState(false);
   const productsList = products.map((product) => {
     return (
       <Product
@@ -25,7 +29,16 @@ function ShoppingList({
 
   return (
     <section className="border max-w-screen-md mx-auto flex flex-col items-center p-10">
-      <ul className="w-4/5 p-4 text-lg">{productsList}</ul>
+      {isLoading ? (
+        <Spinner
+          className="relative left-1/2 top-1/4"
+          name="ball-scale-ripple-multiple"
+          color="var(--color-accent)"
+        />
+      ) : (
+        <ul className="w-4/5 p-4 text-lg">{productsList}</ul>
+      )}
+
       <form className="w-4/5 p-4 flex gap-4" onSubmit={addProduct}>
         <input
           className="border w-full bg-light p-2 focus:outline-none
